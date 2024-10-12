a1d12ff65eafbb7955fe5d799c30ee0bfd1c5192
Nakib Abedin <nakibabedin@gmail.com>
2024-10-12 17:55:06 -0400

    add helper function to parse symbols

--- starter_code.py
+++ starter_code.py
@@ -17,11 +17,13 @@ class Strategy:
     # appends a fair_value column to the pandas dataframe
     self.options = self.calculate_fair_value( self.options )
     self.moving_averages = defaultdict(deque)
+    self.positions = defaultdict(int)
 
     self.underlying = pd.read_csv("data/underlying_data_hour.csv")
     self.underlying.columns = self.underlying.columns.str.lower()
   
 
+  # helper function
   def find_size_weighted_mid(self, bid_price : float, bid_size : int, ask_price : float, ask_size : int ) -> float:
     # will find the average of all of the best bids and offers
     # this can serve as a reasonable fair value estimate
@@ -29,6 +31,7 @@ class Strategy:
     total_price = (bid_price * bid_size) + (ask_price * ask_size)
     return total_price / num_orders
 
+  # helper function
   def calculate_fair_value( self, order_book : pd.DataFrame ) -> pd.DataFrame :
     # use a size-weighted mid between Best Bid and Best Offer at a given time
     # for each order in the order_book, store the fair value for future computations
@@ -36,10 +39,34 @@ class Strategy:
       float(order_book["bid_px_00"]), int(order_book["bid_sz_00"]), 
       float(order_book["ask_px_00"], int(order_book["ask_sz_00"]) ))
     return order_book
+  
+
+  # helper function
+  # given a symbol, will parse and return a dictionary with more friendly formatting of data
+  def parse_symbol( symbol : str ) -> dict:
+    _ , data = symbol.split(" ")
+    parsed_symbol = {}
+
+    # check for the type
+    if "C" in data:
+      parsed_symbol["option_type"] = "C"
+    else:
+      parsed_symbol["option_type"] = "P" 
+    
+    # read in the date
+    expiration_date = data[:8]
+    parsed_symbol["expiration_date"] = expiration_date 
+    
+    # read in the strike price
+    strike_price = data[9:]
+    parsed_symbol["strike_price"] = strike_price
+
+    return parsed_symbol
+
 
   def generate_orders(self) -> pd.DataFrame:
     # # implement me!
-    # example strategy below
+    # moving avg strategy --> NOT FULLY FUNCTIONAL
 
     try:
       my_orders = pd.read_csv("data/example_orders.csv")
@@ -53,12 +80,16 @@ class Strategy:
         if len(self.moving_averages[row.symbol]) > 10:
           self.moving_averages[row.symbol].popleft()
 
-        if curr_moving_average < row.fair_value+5:
-          buy_order = [ row.ts_recv, row.symbol, "B", 10 ]
+        if curr_moving_average > row.fair_value+5:
+          buy_order = [ row.ts_recv, row.symbol, "B", row.bid_sz_00 ]
           my_orders = my_orders.concat( buy_order, ignore_index=True )
-        elif curr_moving_average > row.fair_value-5:
-          sell_order = [ row.ts_recv, row.symbol, "S", 10 ]
+          self.positions += (int(row.bid_sz_00))
+        elif curr_moving_average < row.fair_value-5:
+          sell_order = [ row.ts_recv, row.symbol, "S", row.ask_sz_00 ]
           my_orders = my_orders.concat( sell_order, ignore_index=True )
+          self.positions -= (int(row.ask_sz_00))
+
+
     except:
       with open("errors.txt", "a") as file:
         file.write("we just had an error")
