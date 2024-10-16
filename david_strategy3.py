2673accc6ef8f5e01fee145e4ea62ac1ca38cc36
David <lairdavid1@gmail.com>
2024-10-15 21:09:25 -0400

    Updated strategies.

--- david_strategy3.py
+++ david_strategy3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import helper
 import pricing
+from heapq import heappush, heappop
 from collections import deque
 from datetime import datetime
 
@@ -28,28 +29,56 @@ class Strategy:
         # moving average of s&p data
         self.moving_avg = deque()
 
+        self.open_orders = {}
+
 
     def generate_orders(self) -> pd.DataFrame:
 
         orders = []
-        prev_time = ""
         print("Generating orders...")
         c = 0
         for row in self.options.itertuples():
 
+            # update moving average of underlying hourly data
             prev_hour = helper.update_hour(row.ts_recv)
-            if (prev_hour not in self.underlying or c >= 5000):
+
+            # TODO: Change c when submitting
+            # if (prev_hour not in self.underlying or prev_hour == "2024-03-01 09:30:00-05:00"):
+            if (prev_hour not in self.underlying or c == 20000):
                 orders = pd.DataFrame(orders)
                 orders.to_csv("orders.csv", index=False)
                 print("Orders generated 1")
                 return orders
+
+            # Update moving average
             prev_hour_data = self.underlying[prev_hour]
             mid = (prev_hour_data["high"] + prev_hour_data["low"])/2
-
             self.moving_avg.append(mid)
             if (len(self.moving_avg) > 10):
                 self.moving_avg.popleft()
 
+            # Check if any call options are in the money
+            # tuple: (total size, total price)
+
+            if (row.symbol in self.open_orders and self.open_orders[row.symbol][0] > 0):
+                avg_price = self.open_orders[row.symbol][1]/self.open_orders[row.symbol][0]
+                if (row.bid_px_00 > avg_price):
+                    order = {
+                        "datetime" : row.ts_recv,
+                        "option_symbol" : row.symbol,
+                        "action" : "S",
+                        "order_size" : min(row.bid_sz_00, self.open_orders[row.symbol][0])
+                    }
+                    print(f"Selling {row.symbol} at {row.bid_px_00} with average price {avg_price} for {order['order_size']})
+                    orders.append(order)
+                    new_size = self.open_orders[row.symbol][0] - min(order["order_size"], self.open_orders[row.symbol][0])
+                    new_price = self.open_orders[row.symbol][1] - min(order["order_size"], self.open_orders[row.symbol][0])*avg_price
+                    self.open_orders[row.symbol] = (new_size, new_price)
+
+                if (self.open_orders[row.symbol][0] == 0):
+                    del self.open_orders[row.symbol]
+
+            # Update moving
             mid = sum(self.moving_avg)/len(self.moving_avg)
             
             order_data = helper.parse_order(row);
@@ -62,11 +91,11 @@ class Strategy:
             if (order_data["order_type"] == 'C'):
                 time_to_expiry = helper.time_difference_in_years(order_data["date"][:10], order_data["expiry"])
                 expected = pricing.black_scholes_call(mid, order_data["strike"], time_to_expiry)
+
                 """
                 print(f"Current time: {row.ts_recv}, Expiration: {order_data['expiry']}, Years to expiry: {time_to_expiry}")
                 print(f"Stock price: {mid}, Strike price: {order_data['strike']}")
                 print(f"Expected: {expected}, Actual: {order_data['ask_price']}")
-                """
                 """
                 if (expected > order_data["ask_price"] + 10):
                     order = {
@@ -75,50 +104,19 @@ class Strategy:
                         "action" : "B",
                         "order_size" : int(row.ask_sz_00)//4
                     }
+                    print(f"Buying {row.symbol} at {row.ask_px_00} with expected price {expected} for {order['order_size']}")
+                    if (row.symbol in self.open_orders):
+                        new_size = self.open_orders[row.symbol][0] + order["order_size"]
+                        new_price = self.open_orders[row.symbol][1] + (order_data["ask_price"] * order["order_size"])
+                        self.open_orders[row.symbol] = (new_size, new_price)
+                    else:
+                        self.open_orders[row.symbol] = (order["order_size"], order_data["ask_price"] * order["order_size"])
                     orders.append(order)
-                """
 
-                if (expected < order_data["bid_price"] - 10):
-                    order = {
-                        "datetime" : row.ts_recv,
-                        "option_symbol" : row.symbol,
-                        "action" : "S",
-                        "order_size" : int(row.bid_sz_00)//4
-                    }
-                    orders.append(order)
-            """
-
-            else:
-                time_to_expiry = helper.time_difference_in_years(order_data["date"][:10], order_data["expiry"])
-                expected = pricing.black_scholes_put(mid, order_data["strike"], time_to_expiry)
-
-                print(f"Current time: {row.ts_recv}, Expiration: {order_data['expiry']}, Years to expiry: {time_to_expiry}")
-                print(f"Stock price: {mid}, Strike price: {order_data['strike']}")
-                print(f"Expected: {expected}, Actual: {order_data['ask_price']}")
-
-                if (expected > order_data["ask_price"] + 10):
-                    order = {
-                        "datetime" : row.ts_recv,
-                        "option_symbol" : row.symbol,
-                        "action" : "B",
-                        "order_size" : int(row.ask_sz_00)//4
-                    }
-                    orders.append(order)
-                elif (expected < order_data["bid_price"] - 10):
-                    order = {
-                        "datetime" : row.ts_recv,
-                        "option_symbol" : row.symbol,
-                        "action" : "S",
-                        "order_size" : int(row.bid_sz_00)//4
-                    }
-                    orders.append(order)
-            """
             c += 1
 
 
         print("Orders generated 2")
         return pd.DataFrame(orders)
 
-
-if __name__ == "__main__":
-    s = Strategy()
+ 
--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.stats import norm
 from datetime import datetime, timedelta
 
 def update_hour(timestamp_str: str) -> str:
@@ -24,16 +26,31 @@ def update_hour(timestamp_str: str) -> str:
     formatted_date = rounded_timestamp.strftime("%Y-%m-%d %H:%M:%S-05:00")
     return formatted_date
 
+def time_difference_in_years(date1: str, date2: str) -> float:
+    # Parse the first date formatted as "yyyy-mm-dd"
+    date1_parsed = datetime.strptime(date1, "%Y-%m-%d")
+    
+    # Parse the second date formatted as "yymmdd"
+    date2_parsed = datetime.strptime(date2, "%y%m%d")
+    
+    # Calculate the difference in days
+    difference_in_days = abs((date1_parsed - date2_parsed).days)
+    
+    # Convert days to years
+    difference_in_years = difference_in_days / 365.25  # Considering leap years
+    
+    return difference_in_years
+
 def parse_order(row) -> dict:
     """
-        "date" : row["ts_recv"],
         "instrument" : row["instrument_id"],
-        "bid_price" : row["bid_px_00"],
-        "ask_price" : row["ask_px_00"],
         "bid_size" : row["bid_sz_00"],
         "ask_size" : row["ask_sz_00"],
     """
     data = {
+        "bid_price" : row.bid_px_00,
+        "ask_price" : row.ask_px_00,
+        "date" : row.ts_recv,
         "expiry" : row.symbol[6:12],
         "order_type" : row.symbol[12],
         "strike" : float(row.symbol[13:18])
@@ -53,3 +70,8 @@ if (__name__ == "__main__"):
     print(update_hour("2024-01-12T16:59:02.402838204Z"))
     print(update_hour("2024-01-12T17:19:02.402838204Z"))
 
+    print(time_difference_in_years("2024-10-14", "221014"))
+    print(time_difference_in_years("2024-01-01", "220101"))
+    print(time_difference_in_years("2024-10-14", "190101"))
+    print(time_difference_in_years("2000-12-31", "991231"))
+    print(time_difference_in_years("2024-02-28", "220228"))
